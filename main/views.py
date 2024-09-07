1e1e49a039fb01ba64c5cf19e4d42fa78f780755
ResplendentAlex <alexanderwl295@gmail.com>
2024-09-07 22:14:51 +0700

    edit: change views.py dictionary render

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'name' : 'Pan-Pan Self Portrait',
+        'price': 'Rp. 200.000',
+        'quantity': '10',
+        'description': 'A self portrait of Pan-Pan, the panda.'
+    }
+
+    return render(request, "main.html", context)
