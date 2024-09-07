788a45f62b9cf81434250ee76270ee0d501bdfaa
ResplendentAlex <alexanderwl295@gmail.com>
2024-09-07 22:08:32 +0700

    add: models for models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ProductEntry(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    quanity = models.IntegerField()
+
+    @property
+    def is_product_available(self):
+        return self.quantity > 0
