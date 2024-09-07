0aa989c1c19ced1fb043d735c724019fa6204a93
ResplendentAlex <alexanderwl295@gmail.com>
2024-09-08 01:12:53 +0700

    edit: changed quantity to stock in models.py

--- main/models.py
+++ main/models.py
@@ -4,8 +4,8 @@ class ProductEntry(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    quanity = models.IntegerField()
+    stock = models.IntegerField()
 
     @property
     def is_product_available(self):
-        return self.quantity > 0
+        return self.stock > 0
