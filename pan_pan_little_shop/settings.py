5dd0e0a97ed9574f0bb9d4cb501cc6760465d6cd
ResplendentAlex <alexanderwl295@gmail.com>
2024-09-16 00:33:34 +0700

    add: whitenoise to MIDDLEWARE in settings.py

--- pan_pan_little_shop/settings.py
+++ pan_pan_little_shop/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pan_pan_little_shop.urls'
