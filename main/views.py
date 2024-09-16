4bc5e4186f6832308ffb80e3d624df9b10c151ec
ResplendentAlex <alexanderwl295@gmail.com>
2024-09-16 16:54:20 +0700

    add: add_product function in views.py

--- main/views.py
+++ main/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from main.models import Product
 
 def show_main(request):
+    products = Product.objects.all()
+
     context = {
         'name' : 'Pan-Pan Self Portrait',
         'price': 'Rp. 200.000',
         'stock': '10',
-        'description': 'A self portrait of Pan-Pan, the panda.'
+        'description': 'A self portrait of Pan-Pan, the panda.',
+        'products': products
     }
 
     return render(request, "main.html", context)
+
+def add_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect("main:show_main")
+
+    context = {form: form}
+    return render(request, "add_product.html", context)
