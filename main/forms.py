8bbfc39093860033f5775bbffd1727ae48cea8a2
Resplendence <142782002+ResplendentAlex@users.noreply.github.com>
2024-09-17 23:56:54 +0700

    edit: implement user interface design

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,14 @@
 from django.forms import ModelForm
 from main.models import Product
+from django import forms
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'price', 'description', 'stock']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Product Name'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Product Description'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Product Price'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Stock Quantity'}),
+        }
